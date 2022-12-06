e3132d86f34a4ada22433be029ca7ce69214ab77
JLP04 <jasonpeskin@verizon.net>
2022-12-06 17:57:40 -0500

    libticables 1.3.6 (new formula)

---
+++ Formula/libticables.rb
@@ -0,0 +1,43 @@
+class Libticables < Formula
+  desc "TiCables library is a part of the TiLP project"
+  homepage "http://lpg.ticalc.org/prj_tilp"
+  url "https://github.com/debrouxl/tilibs/archive/12ff32e7bfd7652efae4b5d867bf231ab7d1b170.tar.gz"
+  version "1.3.6"
+  sha256 "720b07e811bcf2be8e6e5897e1d8501e7f3259bfa8b834af2cc1c7b6fbaeed69"
+  license "GPL-2.0-or-later"
+  head "https://github.com/debrouxl/tilibs.git", branch: "master"
+  livecheck do
+    skip "Based on git commits, version number doesn't change"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "gettext" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "tfdocgen" => :build
+  depends_on "glib"
+  depends_on "libusb"
+
+  def install
+    Dir.chdir("libticables/trunk")
+    system "autoreconf", "-i", "-f"
+    system "./configure", *std_configure_args, "--disable-silent-rules", "--enable-libusb10"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <tilp2/ticables.h>
+      int main() {
+        ticables_library_init();
+        ticables_library_exit();
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.cpp", "-L#{lib}", "-lticables2", "-o", "test"
+    system "./test"
+  end
+end
