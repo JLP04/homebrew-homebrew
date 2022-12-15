76ee4728195c42965514cbecacf6ca54abc4f770
JLP04 <jasonpeskin@verizon.net>
2022-12-15 09:07:59 -0500

    libticonv 1.1.6 (new formula)

---
+++ Formula/libticonv.rb
@@ -0,0 +1,42 @@
+class Libticonv < Formula
+  desc "TiConv library is a part of the TiLP project"
+  homepage "http://lpg.ticalc.org/prj_tilp"
+  url "https://github.com/debrouxl/tilibs/archive/12ff32e7bfd7652efae4b5d867bf231ab7d1b170.tar.gz"
+  version "1.1.6"
+  sha256 "720b07e811bcf2be8e6e5897e1d8501e7f3259bfa8b834af2cc1c7b6fbaeed69"
+  license "GPL-2.0-or-later"
+  head "https://github.com/debrouxl/tilibs.git", branch: "master"
+  livecheck do
+    skip "Based on git commits, version number doesn't change"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "gettext" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "tfdocgen" => :build
+  depends_on "glib"
+
+  def install
+    Dir.chdir("libticonv/trunk")
+    system "autoreconf", "-i", "-f"
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <tilp2/ticonv.h>
+      int main() {
+        ticonv_version_get();
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lticonv", "-o", "test"
+    system "./test"
+  end
+end
