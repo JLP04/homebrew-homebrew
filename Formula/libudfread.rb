f161f334fb00078b3df516c039afe2b44175a2b8
JLP04 <jasonpeskin@verizon.net>
2023-07-25 10:52:22 -0400

    libudfread 1.1.2 (new formula)

---
+++ Formula/libudfread.rb
@@ -0,0 +1,45 @@
+class Libudfread < Formula
+  desc "Library for reading UDF from raw devices and image files"
+  homepage "https://code.videolan.org/videolan/libudfread/"
+  url "https://code.videolan.org/videolan/libudfread/-/archive/b3e6936a23f8af30a0be63d88f4695bdc0ea26e1/libudfread-b3e6936a23f8af30a0be63d88f4695bdc0ea26e1.tar.gz"
+  version "1.1.2"
+  sha256 "990c672d52d49af5513d7950a71b6b16791d1111dcfaabcde82e4ff3c7715999"
+  license "LGPL-2.1-only"
+  head "https://code.videolan.org/videolan/libudfread.git", branch: "master"
+  livecheck do
+    url :head
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :test
+
+  def install
+    system "autoreconf", "-i", "-f"
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <udfread/udfread.h>
+
+      int main() {
+        udfread *udf;
+
+        udf = udfread_init();
+        if (!udf) {
+          fprintf(stderr, "udfread_init() failed\\n");
+          return -1;
+        }
+        udfread_close(udf);
+      }
+    EOS
+    flags = shell_output("pkg-config --cflags --libs libudfread").chomp.split
+    system ENV.cc, "-Os", "-g", "-Wall", "-W", "test.c", *flags, "-o", "test"
+    system "./test"
+  end
+end
