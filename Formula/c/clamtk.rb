9dc4cbcf52caf13c173cb96c422f46e2b5a24e7a
JLP04 <jasonpeskin@verizon.net>
2023-08-20 16:40:40 -0400

    clamtk: improve tests
    Closes #36.
    
    Signed-off-by: Jason Peskin <63068340+JLP04@users.noreply.github.com>

--- Formula/c/clamtk.rb
+++ Formula/c/clamtk.rb
@@ -23,15 +23,151 @@ class Clamtk < Formula
   end
   option "with-perlbrew", "Install using perlbrew's perl"
 
+  depends_on "fontconfig" => :build
+  depends_on "freetype" => :build
+  depends_on "libpng" => :build
+  depends_on "libx11" => :build
+  depends_on "libxau" => :build
+  depends_on "libxcb" => :build
+  depends_on "libxdmcp" => :build
+  depends_on "libxext" => :build
+  depends_on "libxrender" => :build
+  depends_on "pixman" => :build
+  depends_on "pkg-config" => :build
+  depends_on "vtk" => :build
+  depends_on "xorgproto" => :build
+  depends_on "cairo"
   depends_on "clamav"
+  depends_on "glib"
+  depends_on "gobject-introspection"
+  depends_on "gtk+3"
   depends_on "perl"
 
+  if build.without? "perlbrew"
+
+    resource "ExtUtils::Depends" do
+      url "https://cpan.metacpan.org/authors/id/X/XA/XAOC/ExtUtils-Depends-0.8001.tar.gz"
+      sha256 "673c4387e7896c1a216099c1fbb3faaa7763d7f5f95a1a56a60a2a2906c131c5"
+    end
+
+    resource "ExtUtils::PkgConfig" do
+      url "https://cpan.metacpan.org/authors/id/X/XA/XAOC/ExtUtils-PkgConfig-1.16.tar.gz"
+      sha256 "bbeaced995d7d8d10cfc51a3a5a66da41ceb2bc04fedcab50e10e6300e801c6e"
+    end
+
+    resource "Glib" do
+      url "https://cpan.metacpan.org/authors/id/X/XA/XAOC/Glib-1.3293.tar.gz"
+      sha256 "7316a0c1e7cc5cb3db7211214f45d7bdc2354365a680ac4bd3ac8bf06d1cb500"
+    end
+
+    resource "LWP::UserAgent" do
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/libwww-perl-6.72.tar.gz"
+      sha256 "e9b8354fd5e20be207afe23ddd584fcd59bf82998dc077decf684ba1dae5a05d"
+    end
+
+    resource "HTTP::Request" do
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/HTTP-Message-6.44.tar.gz"
+      sha256 "398b647bf45aa972f432ec0111f6617742ba32fc773c6612d21f64ab4eacbca1"
+    end
+
+    resource "Clone" do
+      url "https://cpan.metacpan.org/authors/id/G/GA/GARU/Clone-0.46.tar.gz"
+      sha256 "aadeed5e4c8bd6bbdf68c0dd0066cb513e16ab9e5b4382dc4a0aafd55890697b"
+    end
+
+    resource "URI" do
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/URI-5.21.tar.gz"
+      sha256 "96265860cd61bde16e8415dcfbf108056de162caa0ac37f81eb695c9d2e0ab77"
+    end
+
+    resource "HTTP::Date" do
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/HTTP-Date-6.06.tar.gz"
+      sha256 "7b685191c6acc3e773d1fc02c95ee1f9fae94f77783175f5e78c181cc92d2b52"
+    end
+
+    resource "Try::Tiny" do
+      url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/Try-Tiny-0.31.tar.gz"
+      sha256 "3300d31d8a4075b26d8f46ce864a1d913e0e8467ceeba6655d5d2b2e206c11be"
+    end
+
+    resource "LWP::Protocol::https" do
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/LWP-Protocol-https-6.11.tar.gz"
+      sha256 "0132ddbf03661565ca85050f2a5094fb9263cbbc3ccb1a4d9c41ac9bb083b917"
+    end
+
+    resource "Net::HTTP" do
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/Net-HTTP-6.23.tar.gz"
+      sha256 "0d65c09dd6c8589b2ae1118174d3c1a61703b6ecfc14a3442a8c74af65e0c94e"
+    end
+
+    resource "IO::Socket::SSL" do
+      url "https://cpan.metacpan.org/authors/id/S/SU/SULLR/IO-Socket-SSL-2.083.tar.gz"
+      sha256 "904ef28765440a97d8a9a0df597f8c3d7f3cb0a053d1b082c10bed03bc802069"
+    end
+
+    resource "Net::SSLeay" do
+      url "https://cpan.metacpan.org/authors/id/C/CH/CHRISN/Net-SSLeay-1.92.tar.gz"
+      sha256 "47c2f2b300f2e7162d71d699f633dd6a35b0625a00cbda8c50ac01144a9396a9"
+    end
+
+    resource "Text::CSV" do
+      url "https://cpan.metacpan.org/authors/id/I/IS/ISHIGAKI/Text-CSV-2.03.tar.gz"
+      sha256 "48bbce9f234935a88595618e04dd141628246d650f2a726024df1c137e0b65fb"
+    end
+
+    resource "JSON" do
+      url "https://cpan.metacpan.org/authors/id/I/IS/ISHIGAKI/JSON-4.10.tar.gz"
+      sha256 "df8b5143d9a7de99c47b55f1a170bd1f69f711935c186a6dc0ab56dd05758e35"
+    end
+
+    resource "Locale::gettext" do
+      url "https://cpan.metacpan.org/authors/id/P/PV/PVANDRY/Locale-gettext-1.07.tar.gz"
+      sha256 "909d47954697e7c04218f972915b787bd1244d75e3bd01620bc167d5bbc49c15"
+    end
+
+    resource "Gtk3" do
+      url "https://cpan.metacpan.org/authors/id/X/XA/XAOC/Gtk3-0.038.tar.gz"
+      sha256 "70dc4bf2aa74981c79e15fd298d998e05a92eba4811f1ad5c9f1f4de37737acc"
+    end
+
+    resource "Cairo" do
+      url "https://cpan.metacpan.org/authors/id/X/XA/XAOC/Cairo-1.109.tar.gz"
+      sha256 "8219736e401c2311da5f515775de43fd87e6384b504da36a192f2b217643077f"
+    end
+
+    resource "Cairo::GObject" do
+      url "https://cpan.metacpan.org/authors/id/X/XA/XAOC/Cairo-GObject-1.005.tar.gz"
+      sha256 "8d896444d71e1d0bca3d24e31e5d82bd0d9542aaed91d1fb7eab367bce675c50"
+    end
+
+    resource "Glib::Object::Introspection" do
+      url "https://cpan.metacpan.org/authors/id/X/XA/XAOC/Glib-Object-Introspection-0.050.tar.gz"
+      sha256 "ecf3bbb824df5eed6a3a7fcfd61be9ef448519801badcc82a6e3c4daab0cd763"
+    end
+  end
+
   def install
+    if build.without? "perlbrew"
+      ENV.prepend_create_path "PERL5LIB", "share/perl5/vendor_perl"
+      ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+      resources.each do |res|
+        res.stage do
+          ENV["PERL_MM_USE_DEFAULT"] = "1"
+          system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+          system "make", "PERL5LIB=#{ENV["PERL5LIB"]}"
+          system "make", "install"
+        end
+      end
+    end
     inreplace "lib/App.pm" do |s|
       s.gsub! "/usr/share/pixmaps", HOMEBREW_PREFIX/"share/pixmaps"
       s.gsub! "/.local/share/Trash", "/.Trash" if OS.mac?
     end
-    bin.install "clamtk"
+    bin.install "clamtk" if build.with? "perlbrew"
+    if build.without? "perlbrew"
+      (libexec/"bin").install "clamtk"
+      (bin/"clamtk").write_env_script("#{libexec}/bin/clamtk", PERL5LIB: ENV["PERL5LIB"])
+    end
     man1.install "clamtk.1.gz"
     mkdir "#{share}/applications"
     mkdir "#{share}/metainfo"
@@ -52,16 +188,70 @@ class Clamtk < Formula
   def caveats
     if build.with? "perlbrew"
       <<~EOS
-        If you want to install ClamTk to a perlbrew perl, run the following:
+        If you want to install ClamTk to a perlbrew perl, run the following (you will need to install cairo, pkg-config, and vtk to build the perl modules):
         for d in /opt/perl5/perls/perl-*; do
           sudo mkdir -p "$d"/lib/site_perl/5.*/ClamTk
         done
         sudo cp /usr/local/share/perl5/vendor_perl/ClamTk/*.pm /opt/perl5/perls/perl-*/lib/site_perl/5.*/ClamTk/
+        sudo cpan -i ExtUtils::Depends ExtUtils::PkgConfig Glib LWP::UserAgent HTTP::Request Clone URI HTTP::Date Try::Tiny LWP::Protocol::https Net::HTTP IO::Socket::SSL Net::SSLeay Text::CSV JSON Locale::gettext Gtk3  Cairo Cairo::GObject Glib::Object::Introspection
       EOS
     end
   end
 
   test do
-    system "which", "clamtk"
+    (testpath/"test").write <<~EOS
+      #!#{HOMEBREW_PREFIX}/opt/perl/bin/perl
+      use utf8;
+      $| = 1;
+
+      use lib '.';
+      use ClamTk::Analysis;
+      use ClamTk::App;
+      use ClamTk::Assistant;
+      use ClamTk::GUI;
+      use ClamTk::History;
+      use ClamTk::Icons;
+      use ClamTk::Network;
+      use ClamTk::Prefs;
+      use ClamTk::Results;
+      use ClamTk::Scan;
+      use ClamTk::Schedule;
+      use ClamTk::Settings;
+      use ClamTk::Shortcuts;
+      use ClamTk::Startup;
+      use ClamTk::Update;
+      use ClamTk::Quarantine;
+      use ClamTk::Whitelist;
+
+      use Encode 'decode';
+      use Locale::gettext;
+      use POSIX 'locale_h';
+      textdomain( 'clamtk' );
+      setlocale( LC_ALL, '' );
+
+      setlocale( LC_TIME, 'C' );
+      bind_textdomain_codeset( 'clamtk', 'UTF-8' );
+
+      my $arg = decode ( 'utf8', $ARGV[0] );
+
+      my $trash_dir = ClamTk::App->get_path( 'trash_dir' );
+      if ( $arg eq '//' ) {
+        $arg = $trash_dir;
+      } elsif ( $arg =~ m#^//(.*?)$# ) {
+        my $trash_dir_files = ClamTk::App->get_path( 'trash_dir_files' );
+        if ( -e "$trash_dir_files/$1" ) {
+          $arg = "$trash_dir_files/$1";
+        }
+      }
+
+      ClamTk::Prefs->structure;
+
+      ClamTk::Prefs->custom_prefs;
+    EOS
+    inreplace "test", "#!#{HOMEBREW_PREFIX}/opt/perl/bin/perl", "#!/usr/bin/env perl" if build.with? "perlbrew"
+    ENV.prepend_create_path "PERL5LIB", "#{share}/perl5/vendor_perl" if build.without? "perlbrew"
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+    chmod "+x", "test"
+    system "./test"
   end
 end
