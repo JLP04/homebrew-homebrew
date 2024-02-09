be8c280ba07d1140f41c861402a3d99be8f79c19
JLP04 <jasonpeskin@verizon.net>
2024-02-09 13:32:17 -0500

    clamtk: update perl dependencies

--- Formula/c/clamtk.rb
+++ Formula/c/clamtk.rb
@@ -8,7 +8,7 @@ class Clamtk < Formula
     "BSD-3-Clause",
     any_of: ["GPL-1.0-or-later", "Artistic-2.0"],
   ]
-  revision 12
+  revision 13
   head "https://gitlab.com/dave_m/clamtk.git", branch: "master"
   livecheck do
     url :stable
@@ -77,8 +77,8 @@ class Clamtk < Formula
     end
 
     resource "URI" do
-      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/URI-5.26.tar.gz"
-      sha256 "622bb95f588e25923a504c62687f4c8c7d7c3e0cc247449edf146228e5ae686f"
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/URI-5.27.tar.gz"
+      sha256 "11962d8a8a8496906e5d34774affc235a1c95c112d390c0b4171f3e91e9e2a97"
     end
 
     resource "HTTP::Date" do
