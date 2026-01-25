9dc171de1e3111bc88e47a120a02f9135efd8a70
JLP04 <jasonpeskin@verizon.net>
2026-01-25 10:46:27 -0500

    tilp 1.19_6
    Closes #156.
    
    Signed-off-by: Jason Peskin <63068340+JLP04@users.noreply.github.com>

--- Formula/t/tilp.rb
+++ Formula/t/tilp.rb
@@ -1,11 +1,11 @@
 class Tilp < Formula
   desc "Program allowing a computer to communicate with TI graphing calculators"
   homepage "http://lpg.ticalc.org/prj_tilp"
-  url "https://github.com/debrouxl/tilp_and_gfm/archive/0a525619a07d92734b5eb5ba1d47c56f4de37458.tar.gz"
+  url "https://github.com/debrouxl/tilp_and_gfm/archive/4a399bc491b2d2d9fdab76d23d7cf87a43b0beec.tar.gz"
   version "1.19"
-  sha256 "fd638afc5eb7104be54d465137c22fafb024cef465837691fe66234d0d429513"
+  sha256 "33347790504b25a5b33bdf64f950ff86584e7668644a5f275441348e06b763c3"
   license "GPL-2.0-or-later"
-  revision 5
+  revision 6
   head "https://github.com/debrouxl/tilp_and_gfm.git", branch: "master"
   livecheck do
     skip "Based on git commits, version number doesn't change"
