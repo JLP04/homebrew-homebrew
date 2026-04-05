1c9ce30b2555e4e4d869cbd9c8c91e70eddee99d
Jason Peskin <63068340+JLP04@users.noreply.github.com>
2026-04-05 22:03:18 +0000

    tfdocgen: update 1.00 bottle.

--- Formula/t/tfdocgen.rb
+++ Formula/t/tfdocgen.rb
@@ -13,11 +13,12 @@ class Tfdocgen < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/jlp04/homebrew"
-    rebuild 11
-    sha256 cellar: :any,                 arm64_tahoe:   "bb5fca46a58926f6c1e09a76d3214def98842d9146c57ce803576d9281b9098b"
-    sha256 cellar: :any,                 arm64_sequoia: "76b0ca6cb7028b0578b3d6e1ea599178518e23d657884f2b6a296b68a63b7e5d"
-    sha256 cellar: :any,                 arm64_sonoma:  "1d264931ba3c7064ee3b694f0317ac8250c6bcfe93c5ce5c6b948af56b03532f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1816d1cf0baed51929fe77290a2ddb1e75148bdec462353504002424952178c8"
+    rebuild 12
+    sha256 cellar: :any,                 arm64_tahoe:   "af6b95d5fc72080cb9e5074736a2bc9105ca822c3038279818e15b2b0892fbe8"
+    sha256 cellar: :any,                 arm64_sequoia: "5e35387396e98d1bb06bb8a8d145f79f10a12db69d7bcf4406548496ebc8d781"
+    sha256 cellar: :any,                 arm64_sonoma:  "9542bc72dbbbf315d681c4de61bcbf4995a8e3cc0fbc93ee6eccf5e2fb6c0298"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0d12634cd10675fecdcd4231a8adaf422dafdbee3adff9c2b5f70e21445f885c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3872684264e0e6f557e87561611ca36ca2b2317b820da38577019e9df5a78495"
   end
 
   depends_on "autoconf" => :build
