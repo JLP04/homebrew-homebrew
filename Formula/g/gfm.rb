9d37f5fd9bc5aa47ee0994ac0186baf0c8da37d3
JLP04 <jasonpeskin@verizon.net>
2026-09-12 17:41:22 -0400

    gfm: remove extra whitespace to trigger a build for macOS 27

--- Formula/g/gfm.rb
+++ Formula/g/gfm.rb
@@ -11,7 +11,7 @@ class Gfm < Formula
   livecheck do
     skip "Based on git commits, version number doesn't change"
   end
- 
+
   bottle do
     root_url "https://ghcr.io/v2/jlp04/homebrew"
     rebuild 4
